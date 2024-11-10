df5b19f0567c288ec6a2942f95ebd1c48025d45c
MasterVVK <vvk@2140101.ru>
2024-11-10 22:15:36 +0500

    Up

---
+++ voice.py
@@ -0,0 +1,21 @@
+from elevenlabs import save
+from elevenlabs.client import ElevenLabs
+import config
+
+client = ElevenLabs(
+    api_key=config.elevenlabs_api_key,
+)
+
+def get_all_voices():
+    voices = client.voices.get_all()
+    return [{'name': voice.name, 'id': voice.voice_id} for voice in voices.voices]
+
+def generate_audio(text: str, voice: str):
+    audio = client.generate(
+        text=text,
+        voice=voice,
+        model="eleven_multilingual_v2"
+    )
+    name = "audio.mp3"
+    save(audio, name)
+    return name
